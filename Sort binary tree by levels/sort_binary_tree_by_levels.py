a73b056649bb3996ed5a97af429a6847f84e5766
Tanya <morozt208@gmail.com>
2025-05-01 17:49:59 +0300

    sort binary tree  by levels passed

---
+++ Sort binary tree by levels/sort_binary_tree_by_levels.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+class Node:
+    def __init__(self, L, R, n):
+        self.left = L
+        self.right = R
+        self.value = n
+
+def tree_by_levels(node):
+    if not node:
+        return []
+    
+    result = []
+    queue = deque([node])
+    
+    while queue:
+        current = queue.popleft()
+        result.append(current.value)
+        if current.left:
+            queue.append(current.left)
+        if current.right:
+            queue.append(current.right)
+    
+    return result
